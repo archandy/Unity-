4f4fe50f9f9c7556ba2c260950df95e6427acae1
archandy <archandy85@gmail.com>
2015-05-12 08:46:20 +0900

    EJ_BoardGame
    boardgame

---
+++ BoardGame/Assets/BarrelScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrelScript : MonoBehaviour {
+	public bool IsClick = false;
+	public bool IsFail = false;
+	// Use this for initialization
+	void Start () {}
+	
+	// Update is called once per frame
+	void Update () {
+		if (IsClick == true) {
+			if(IsFail == false){
+			gameObject.transform.Rotate (0, 1, 0);
+			}else if(IsFail ==true){
+				//print ("Fail!");
+				Application.LoadLevel ("GameOverScene");
+			}
+		}
+	}
+}
