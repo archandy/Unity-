5eab8be2d6ae6ef67c1e37fff51905d7ec58ca4f
archandy <archandy85@gmail.com>
2015-05-13 21:17:05 +0900

    EJ_BasicControl
    basic control

---
+++ EJ_Basic/Assets/movementCtrl.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var jumpHeight = 18;
+private var isFalling=false;
+
+
+function Update () {
+	//handle object jumping
+	if(Input.GetKeyDown(KeyCode.Space)&&isFalling==false){
+		GetComponent.<Rigidbody>().velocity.y=jumpHeight;
+		isFalling=true;
+		isFalling=false;
+	}
+	if(Input.GetKeyDown(KeyCode.A)){
+		GetComponent.<Rigidbody>().velocity.x=-1;
+	}
+	if(Input.GetKeyDown(KeyCode.D)){
+		GetComponent.<Rigidbody>().velocity.x=1;
+	}
+	if(Input.GetKeyDown(KeyCode.S)){
+		GetComponent.<Rigidbody>().velocity.z=-1;
+	}
+	if(Input.GetKeyDown(KeyCode.W)){
+		GetComponent.<Rigidbody>().velocity.z=1;
+	}
+} 
